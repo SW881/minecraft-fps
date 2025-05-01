fcaf62a1233be970462670006f31432c91276068
shubh <shubhamw881@gmail.com>
2025-05-01 23:06:06 +0530

    fix: updated colliders

--- src/components/Player1.jsx
+++ src/components/Player1.jsx
@@ -6,9 +6,6 @@ import * as RAPIER from "@dimforge/rapier3d-compat"
 
 import usePersonControls from "../hooks/PlayerHooks.js";
 import Weapon from "./Weapon.jsx";
-import { Wireframe } from "@react-three/drei";
-import { Character1 } from "./character-components/Character1.jsx";
-import { Character2 } from "./character-components/Character2.jsx";
 import VoxelCharacter from "./character-components/VoxelCharacter.jsx";
 
 const direction = new THREE.Vector3();
@@ -16,31 +13,33 @@ const frontVector = new THREE.Vector3();
 const sideVector = new THREE.Vector3();
 const rotation = new THREE.Vector3();
 let runAudio = new Audio('/player_walk.mp3');
-
-// sunLight.shadow.normalBias = 0.2;
+const accelerationFactor = 0.9; // Adjust for responsiveness (0 to 1)
 
 const Player1 = () => {
     const { camera } = useThree();
     const playerRef = useRef();
     const headRef = useRef();
-    const headColliderRef = useRef();
-    const objectInHandRef = useRef();
+    const midBodyRef = useRef();
+    const legRef = useRef();
     const swayingObjectRef = useRef();
+    const objectInHandRef = useRef();
     const [moveSpeed, setMoveSpeed] = useState(20);
+    const [rigidBodyPosition, setRigidBodyPosition] = useState([-114, 50, -18.16])
     const [playerHeight, setHeight] = useState(2);
     let { jump, backward, left, right, forward, crouch } = usePersonControls();
     const rapier = useRapier();
     const [isOnFloor, setIsOnFloor] = useState(false);
     const [maxJump, setMaxJump] = useState(6);
-    const [headRotation, setHeadRotation] = useState([0, 0, 0]);
+    const [headRotation, setHeadRotation] = useState();
+    const [bodyRotation, setBodyRotation] = useState([0, 0, 0])
 
     useEffect(() => {
         // Set the aspect ratio once when the component is mounted
         camera.fov = 60;
         camera.aspect = 1920 / 1080; // Set fixed aspect ratio to 4:3
         camera.updateProjectionMatrix(); // Update the camera matrix to apply changes
-        // camera.far = 1000;
-        // camera.near = 0.1;
+        camera.far = 1000;
+        camera.near = 0.1;
     }, [camera]);
 
     useFrame((state) => {
@@ -50,22 +49,28 @@ const Player1 = () => {
 
         frontVector.set(0, 0, backward - forward);
         sideVector.set(left - right, 0, 0);
-        direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(moveSpeed).applyEuler(state.camera.rotation);
+        direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(10).applyEuler(state.camera.rotation);
 
         if ((backward || left || right || forward) && !crouch && !playerRef.current.linvel() <= 0) {
             playRunSound();
         }
 
+        const targetVelocity = new THREE.Vector3(direction.x, velocity.y, direction.z);
+        const newVelocity = new THREE.Vector3();
+
+        newVelocity.lerpVectors(velocity, targetVelocity, accelerationFactor);
+
+        // console.log({ newVelocity });
         playerRef.current.wakeUp();
-        playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
+        setRigidBodyPosition(velocity);
+        playerRef.current.setLinvel(newVelocity);
+
+        // playerRef.current.wakeUp();
+        // playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
 
         const world = rapier.world;
         const rayOrigin = playerRef.current ? playerRef.current.translation() : null;
         const ray = world.castRay(new RAPIER.Ray(rayOrigin, { x: 0, y: -1, z: 0 }));
-        // console.log("Ray Origin:", rayOrigin);
-        // console.log("Raycast Hit:", ray);
-        // console.log('Player Ref :', playerRef.current.translation().y);
-        // console.log('Ray Origin :', rayOrigin.y);
 
         let currentDistance = playerRef.current.translation().y - rayOrigin.y
         // console.log({ currentDistance })
@@ -73,7 +78,7 @@ const Player1 = () => {
         // Jump logic
         if (jump && isOnFloor && maxJump >= 0) {
             setMaxJump(maxJump - 1)
-            console.log({ maxJump })
+            // console.log({ maxJump })
             doJump();
         } else if (maxJump <= 0) {
             // console.log(`Can't Jump more..`)
@@ -83,8 +88,8 @@ const Player1 = () => {
         }
 
         const { x, y, z } = playerRef.current.translation();
-        // state.camera.position.set(x, y + 1.5, z + 1);
-        state.camera.position.set(x, y + 1.5, z + 2);
+        // state.camera.position.set(x, y + 1.5, z - 0.4); // Test position 1
+        state.camera.position.set(x, y + 1.5, z + 4); // Test position 1
 
         if (crouch) {
             setMoveSpeed(8);
@@ -95,17 +100,50 @@ const Player1 = () => {
         }
 
         objectInHandRef.current.rotation.copy(state.camera.rotation);
-        const cameraRotation = camera.getWorldQuaternion(new THREE.Quaternion());
-        headRef.current.setRotation(cameraRotation);
-        console.log('Collider Head rotation : ', cameraRotation);
-        objectInHandRef.current.position.copy(state.camera.position).add(state.camera.getWorldDirection(rotation));
+        const cameraQuaternionRotation = camera.getWorldQuaternion(new THREE.Quaternion());
+        // console.log('Camera Y rotation : ', camera.rotation.y);
+        // console.log({ cameraQuaternionRotation });
+        // const cameraEuler = new THREE.Euler().setFromQuaternion(cameraQuaternionRotation);
+        headRef.current.setRotation(cameraQuaternionRotation);
+        // midBodyRef.current.setRotation(cameraQuaternionRotation)
 
-        // console.log({ cameraRotation });
-        setHeadRotation(cameraRotation);
+        if (midBodyRef.current) {
+            // const currentQuaternion = midBodyRef.current.quaternion.clone();
+            const cameraEuler = new THREE.Euler().setFromQuaternion(cameraQuaternionRotation);
+            // console.log({ cameraEuler });
+            // console.log("Camera Rotation  : ", camera.rotation)
+            midBodyRef.current.setRotation(cameraEuler);
+            let y = cameraEuler.y;
+            if (0 <= y && y <= 1.5) {
+                y = 0.75
+            } else if (0 >= y && y <= -1.5) {
+                y = -0.75
+            } else if (0 >= y && y <= 1.5) {
+                y = 0.75
+            } else if (0 >= y && y <= -1.5) {
+                y = -0.75
+            }
+
+            setBodyRotation([0, y, 0]);
+            console.log('Euler Y : ', cameraEuler.y);
+            // console.log({ bodyRotation });
+            // const yAxisQuaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), cameraEuler.y);
+
+            // Combine the current X and Z rotations with the new Y rotation
+            // const currentXZ = new THREE.Quaternion().setFromEuler(new THREE.Euler(currentQuaternion.toEuler().x, 0, currentQuaternion.toEuler().z));
+            // midBodyRef.current.quaternion.copy(new THREE.Quaternion().multiplyQuaternions(currentXZ, yAxisQuaternion));
+            // midBodyRef.current.matrixAutoUpdate = true;
+        }
+        // legRef.current.setRotation(cameraQuaternionRotation);
+
+        // console.log('Leg Ref Current : ', legRef.current.rotation);
+
+        objectInHandRef.current.position.copy(state.camera.position).add(state.camera.getWorldDirection(rotation));
+        setHeadRotation(cameraQuaternionRotation);
     });
 
     const playRunSound = () => {
-        // runAudio.play();
+        runAudio.play();
     }
 
     const doJump = () => {
@@ -115,25 +153,22 @@ const Player1 = () => {
     return (
         <>
             <group>
-                <RigidBody position={[-114, 80, -18.16]} ref={playerRef} lockRotations={true} type="kinematic-position"
+                <RigidBody position={[-114, 50, -18.16]} ref={playerRef} lockRotations={true} type="dynamic"
                     onCollisionEnter={(other) => {
                         if (other.rigidBodyObject.name === "ground") {
-                            // console.log('Rigid Body Object :', other.rigidBodyObject)
                             setIsOnFloor(true)
-                            // console.log('Colliding with ground...')
                         }
                     }}
                     onCollisionExit={(other) => {
                         if (other.rigidBodyObject.name === "ground") {
-                            // console.log('Rigid Body Object :', other.rigidBodyObject)
                             setIsOnFloor(false)
-                            // console.log('Not Colliding with ground...')
                         }
                     }}>
-                    <CuboidCollider args={[0.5, 0.5, 0.5]} position={[0, 1.5, 0]} ref={headRef} />
-                    <CuboidCollider args={[0.5, 0.65, 0.32]} position={[0, 0.35, 0]} />
-                    <CuboidCollider args={[0.5, 0.8, 0.4]} position={[0, -1.1, 0]} />
-                    <VoxelCharacter hRotate={headRotation} />
+                    <CapsuleCollider args={[0.1 / 2, 0.28]} position={[0, 1.40, 0]} ref={headRef} />
+                    {/* <CapsuleCollider args={[0.3, 0.35]} position={[0, 0.6, -0.01]} ref={midBodyRef} /> */}
+                    <CuboidCollider args={[0.32, 0.5, 0.19]} position={[0, 0.58, 0.012]} ref={midBodyRef} />
+                    <CapsuleCollider args={[0.4, 0.3]} position={[0, -0.5, 0]} ref={legRef} />
+                    {headRotation && <VoxelCharacter hRotate={headRotation} />}
                 </RigidBody>
             </group>
             <group ref={objectInHandRef}>
